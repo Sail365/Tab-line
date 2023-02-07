88e179ed3026dbbba3d6ac10113ce0a87efffed6
Gilin3000 <lotus0110@nate.com>
2023-02-08 08:59:29 +0900

    feat : add home, member

---
+++ demo/src/test/java/tab_line/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tab_line.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
