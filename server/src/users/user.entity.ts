a9362f2a6d61fe8744eabfc7307628fa61240365
Gilin3000 <lotus0110@nate.com>
2023-03-01 00:01:42 +0900

    feat : user entity

---
+++ server/src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Schema, Prop } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  id: number;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  password: string;
+}
