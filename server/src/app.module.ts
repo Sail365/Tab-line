b56f842f9c20c554268b9b85d3d55d997e6f403d
Gilin3000 <lotus0110@nate.com>
2023-03-03 18:13:38 +0900

    feat : config module global

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI, {
       useNewUrlParser: true,
     }),
